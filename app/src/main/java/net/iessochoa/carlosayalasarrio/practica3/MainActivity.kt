17846570da8a9d0bf08d43191ceb3e8b3fd3870a
Carlos <CarlosAyalaSarrio18@gmail.com>
2024-02-24 23:10:38 +0100

    Final de Asteriscos terminado

--- app/src/main/java/net/iessochoa/carlosayalasarrio/practica3/MainActivity.kt
+++ app/src/main/java/net/iessochoa/carlosayalasarrio/practica3/MainActivity.kt
@@ -4,6 +4,8 @@ import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
@@ -15,25 +17,30 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     //var num: Int = 0
+
     val model:MainActivityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // binding.tvNumero.text=model.contador.toString()
+        //binding.tvNumero.text=model.contador.toString()
 
         model.getContador().observe(this, Observer<Int>{
-
-            cont->binding.tvNumero.text = cont.toString()
+                cont->binding.tvNumero.text=cont.toString()
         })
-        binding.btSumaUno.setOnClickListener(){
 
+        model.asteriscos.observe(this) { asteriscos ->
+            binding.tvAsteriscos.text = asteriscos
+        }
+
+        binding.btSumaUno.setOnClickListener(){
             model.sumaUno()
-           // binding.tvNumero.text=model.contador.toString()
+            model.addAsteriscos()
+
+            //binding.tvNumero.text=model.contador.toString()
         }
     }
-
         override fun onStart() {
             super.onStart()
             Log.i(TAG, "onStart")
--- app/src/main/java/net/iessochoa/carlosayalasarrio/practica3/MainActivityViewModel.kt
+++ app/src/main/java/net/iessochoa/carlosayalasarrio/practica3/MainActivityViewModel.kt
@@ -8,14 +8,19 @@ class MainActivityViewModel: ViewModel() {
 
     //var contador:Int=0
 
-    private val contador = MutableLiveData<Int>(0)
+    private val _asteriscos = MutableLiveData<String>("")
+    val asteriscos: LiveData<String> = _asteriscos
 
+    private val contador=MutableLiveData<Int>(0)
     fun sumaUno(){
         contador.value= contador.value?.plus(1)
     }
 
-    fun getContador(): LiveData<Int>{
-        return  contador
+    fun getContador():LiveData<Int>{
+        return contador
     }
 
+    fun addAsteriscos() {
+        _asteriscos.value = _asteriscos.value + "*"
+    }
 }
